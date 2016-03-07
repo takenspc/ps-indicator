e9ce4a2e99f20027ac4662b316982e253da4d8b0
Takeshi Kurosawa <taken.spc@gmail.com>
2016-03-07 21:22:19 +0900

    Use real platform status data

--- background/background.js
+++ background/background.js
@@ -3,10 +3,33 @@
 /*
  *
  */
+class DataStore {
+    constructor(data) {
+        this.url2entry = new Map();
+
+        for (const specEntry of data) {
+            if (specEntry.url === '') {
+                continue;
+            }
+
+            this.url2entry.set(specEntry.url, specEntry);
+        }
+    }
+
+    query(url) {
+        return this.url2entry.get(url);
+    }
+}
+
+
+
+/*
+ *
+ */
 class TabHandler {
-    constructor() {
+    constructor(store) {
+        this.store = store;
         this.activeTabId = null;
-        this.activeTabURL = null;
         chrome.tabs.onActivated.addListener(this.onActivated.bind(this));
         chrome.tabs.onUpdated.addListener(this.onUpdated.bind(this));
         chrome.runtime.onMessage.addListener(this.onMessage.bind(this));
@@ -18,9 +41,7 @@ class TabHandler {
             this.activeTabId = tabId;
 
             const url = tab.url;
-            this.activeTabURL = url;
-            // console.log('activated', tabId, url);
-            this.updateBrowserAction.bind(this)(tabId, url);
+            this.updateURL(url);
         });
     }
 
@@ -34,25 +55,30 @@ class TabHandler {
         }
 
         const url = changeInfo.url;
-        this.activeTabURL = url;
-        // console.log('updated', tabId, url);
-        this.updateBrowserAction.bind(this)(tabId, url);
+        this.updateURL(url);
     }
 
-    updateBrowserAction(tabId, url) {
+    normalizeURL(url) {
+        const urlObject = new URL(url);
+        urlObject.hash = '';
+        return urlObject.href;
+    }
+
+    updateURL(url) {
+        const normalizedURL = this.normalizeURL(url);
+        this.updateBrowserAction(normalizedURL);
+    }
+
+    updateBrowserAction(url) {
         const data = this.query(url);
         chrome.browserAction.setBadgeText({
-            text: '' + data.length,
-            tabId: tabId,
+            text: (data ? '' + data.fragments.length : ''),
+            tabId: this.activeTabId,
         });
     }
 
     query(url) {
-        const data = [
-            { id: 0, title: "asm.js", url: "http://asmjs.org/spec/latest/", indicators: "Firefox" },
-            { id: 1, title: "Background Sync API", url: "https://wicg.github.io/BackgroundSync/spec/", indicators: "Chrome" },
-        ];
-        return data;
+        return this.store.query(url);
     }
 
     onMessage(message) {
@@ -60,9 +86,11 @@ class TabHandler {
         const type = message.type;
         if (type === 'query') {
             const url = message.url;
+            const normalizedURL = this.normalizeURL(url);
+
             // console.log('recieving query', url);
 
-            const data = this.query(url);
+            const data = this.query(normalizedURL);
 
             // console.log('sending response', url);
             chrome.runtime.sendMessage({
@@ -75,4 +103,30 @@ class TabHandler {
 
 }
 
-const handler = new TabHandler();
+(function() {
+    function readJSON(url) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.responseType = 'json';
+
+            xhr.onload = (data) => {
+                resolve(xhr.response);
+            };
+
+            xhr.onerror = (err) => {
+                reject(err);
+            };
+            
+            xhr.open('GET', url);
+            xhr.send();
+        });
+    }
+
+    const jsonURL = chrome.runtime.getURL('data/data.json');
+    readJSON(jsonURL).then((data) => {
+        const dataStore = new DataStore(data);
+        new TabHandler(dataStore);
+    }).catch((err) => {
+        console.log(err);
+    });
+})();
