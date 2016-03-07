3781bce623e30179231d00a60e455e10603510b5
Takeshi Kurosawa <taken.spc@gmail.com>
2016-03-07 21:20:15 +0900

    Add chrome.d.ts

--- typings/bundle.d.ts
+++ typings/bundle.d.ts
@@ -1,2 +1,6 @@
 /// <reference path="react/react.d.ts" />
 /// <reference path="react/react-dom.d.ts" />
+/// <reference path="chrome/chrome.d.ts" />
+/// <reference path="webrtc/MediaStream.d.ts" />
+/// <reference path="filesystem/filesystem.d.ts" />
+/// <reference path="filewriter/filewriter.d.ts" />
