f8da37bfd93838df765db1204ad26b13bbaf5f20
Takeshi Kurosawa <taken.spc@gmail.com>
2016-03-20 13:54:38 +0900

    Refactor browserAction 1

--- browserAction/index.js
+++ browserAction/index.js
@@ -1,47 +1,67 @@
 'use strict';
-function getURL() {
-    return new Promise(function (resolve, reject) {
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true,
-        }, function (tabs) {
-            const tab = tabs[0];
-            const url = tab.url;
-            resolve(url);
+
+class DataStore {
+    static getData() {
+        return this.getURL().then((url) => {
+            return this.queryData(url);
+        })
+    }
+
+    /**
+     * @private
+     * @returns {Promise<string>}
+     */
+    static getURL() {
+        return new Promise(function(resolve, reject) {
+            chrome.tabs.query({
+                active: true,
+                currentWindow: true,
+            }, function(tabs) {
+                const tab = tabs[0];
+                const url = tab.url;
+                resolve(url);
+            });
         });
-    });
+    }
+
+    /**
+     * @private
+     * @param {string} url
+     */
+    static queryData(url) {
+        return new Promise(function(resolve, reject) {
+            chrome.runtime.onMessage.addListener((message) => {
+                const type = message.type;
+                if (type === 'response' && message.url === url) {
+                    const data = message.data;
+                    resolve(data);
+                }
+            });
+
+            chrome.runtime.sendMessage({
+                type: 'query',
+                url: url,
+            });
+        });
+    }
 }
 
-function queryData(url) {
-    return new Promise(function (resolve, reject) {
-        chrome.runtime.onMessage.addListener((message) => {
-            const type = message.type;
-            if (type === 'response' && message.url === url) {
-                const data = message.data;
-                resolve(data);
-            }
-        });
 
-        chrome.runtime.sendMessage({
-            type: 'query',
-            url: url,
-        });
-    });
-}
+const Indicator = new Vue({
+    el: '#indicator',
+    data: {
+        url: null,
+        fragments: [],
+        engines: ['chromium', 'edge'],
+    },
 
-(function () {
-    getURL().then((url) => {
-        return queryData(url);
-    }).then((specEntry) => {
-        new Vue({
-            el: '#indicator',
-            data: {
-                specEntry: specEntry,
-                engines: ['chromium', 'edge'],
-            },
+    created: function() {
+        DataStore.getData().then((specEntry) => {
+            console.log(this);
+            this.url = specEntry.url;
+            this.fragments = specEntry.fragments;
+        }).catch((err) => {
+            console.error(err);
         });
-    }).catch((err) => {
-        alert(err);
-    });
-
-})();
+    },
+});
